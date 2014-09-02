bbb908452b93f21ebd3d188312e533d626904b42
Your Name <you@example.com>
2014-09-02 14:42:49 +0200

    more configuration

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 setup(name='pythonioc',
       packages=['pythonioc'],
       description='simplistic dependency injection container for python',
-      author = 'Franz Eichhorn',
-      author_email = 'frairon@gmail.com'
-      version='0.1',
-      py_modules=['serviceproxy.py',
-                  'serviceregistry.py'],
+      author='Franz Eichhorn',
+      author_email='frairon@googlemail.com',
+      url='https://bitbucket.org/eh14/python-ioc',
+      license='LICENSE',
+      version='0.1'
       )
